da02122c1fe981086df8f5a160b16923f383a63a
binglecringle180 <binglecringle180@gmail.com>
2024-10-26 12:31:22 -0700

    test

---
+++ script.js
@@ -0,0 +1,47 @@
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas1");
+const file = document.getElementById("fileupload");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d")
+let audioSource;
+let analyser;
+
+file.addEventListener("change", function(){
+    const files = this.files;
+    const audio1 = document.getElementById("audio1");
+    const audioContext = new AudioContext();
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    audio1.play();
+    audioSource = audioContext.createMediaElementSource(audio1);
+    analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 512; //size
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const div = canvas.width/bufferLength;
+    let x;
+    let y;
+
+    function animate(){
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualizer(bufferLength, x, div, y, dataArray);
+        requestAnimationFrame(animate);
+    }
+    animate();
+    
+})
+
+function drawVisualizer(bufferLength, x, div, y, dataArray){
+    for (let i = 0; i < bufferLength; i++){
+        y = dataArray[i] * 2;
+        ctx.fillStyle = "white";
+        ctx.fillRect(x, canvas.height - y, div, y);
+        x += div;
+    }
+}
