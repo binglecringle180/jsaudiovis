51d918b4f13d6ec42d00517d17e5f1c8f9f24202
binglecringle180 <binglecringle180@gmail.com>
2024-10-27 13:49:37 -0700

    i hate mysalf

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const canvas = document.getElementById("canvas1");
 const file = document.getElementById("fileupload");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d");
+let audioContext;
 let audioSource;
 let analyser;
 
 file.addEventListener("change", function(){
     const files = this.files;
     const audio1 = document.getElementById("audio1");
-    const audioContext = new AudioContext();
+    audio1.src = '';
     audio1.src = URL.createObjectURL(files[0]);
-    audio1.pause();
     audio1.load();
     audio1.play();
+    audioContext = new AudioContext();
     audioSource = audioContext.createMediaElementSource(audio1);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
